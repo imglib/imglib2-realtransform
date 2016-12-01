a1f6b3ad26711e681587354c4adc1f86da0d209d
Alison Walter <awalter2@wisc.edu>
2016-12-01 13:28:47 -0600

    Add assertions to ensure inverse transform updated

--- src/test/java/net/imglib2/realtransform/AffineTransform3DTest.java
+++ src/test/java/net/imglib2/realtransform/AffineTransform3DTest.java
@@ -104,8 +104,9 @@ public class AffineTransform3DTest
 
 			dR.concatenate( affine );
 			affine.rotate( axis, d );
-			
+
 			assertArrayEquals( dR.getRowPackedCopy(), affine.getRowPackedCopy(), 0.001 );
+			assertArrayEquals( dR.inverse().getRowPackedCopy(), affine.inverse().getRowPackedCopy(), 0.001 );
 		}
 	}
 	
@@ -131,8 +132,9 @@ public class AffineTransform3DTest
 			
 			dR.concatenate( affine );
 			affine.scale( s );
-			
+
 			assertArrayEquals( dR.getRowPackedCopy(), affine.getRowPackedCopy(), 0.001 );
+			assertArrayEquals( dR.inverse().getRowPackedCopy(), affine.inverse().getRowPackedCopy(), 0.001 );
 		}
 	}
 	
@@ -154,7 +156,8 @@ public class AffineTransform3DTest
 		//Move to origin and test
 		toBeOrigin.translate(inverseTranslation);
 		assertArrayEquals( toBeOrigin.getTranslation(), new double[]{0, 0, 0}, 0.001 );
-		
+		assertArrayEquals( toBeOrigin.inverse().getTranslation(), new double[]{ 0, 0, 0 }, 0.001 );
+
 		//Move to origin the easy way
 		toBeOrigin = affine.copy();
 		toBeOrigin.setTranslation(0, 0, 0);
@@ -165,9 +168,7 @@ public class AffineTransform3DTest
 		backToOriginal.translate(translationFromOrigin);
 
 		assertArrayEquals( backToOriginal.getRowPackedCopy(), affine.getRowPackedCopy(), 0.001 );
-		
-		
-		
+		assertArrayEquals( backToOriginal.inverse().getRowPackedCopy(), affine.inverse().getRowPackedCopy(), 0.001 );
 	}
 
 }
