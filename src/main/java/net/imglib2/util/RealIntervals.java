2873d79018b66e781c48cf0fe0f502c9c0644b6e
bogovicj <bogovicj@janelia.hhmi.org>
2022-04-12 15:59:10 -0400

    feat: bounding box estimation for RealTransformed RealIntervals
    * add a test

---
+++ src/main/java/net/imglib2/util/RealIntervals.java
@@ -0,0 +1,212 @@
+/*-
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2024 Tobias Pietzsch, Stephan Preibisch, Stephan Saalfeld,
+ * John Bogovic, Albert Cardona, Barry DeZonia, Christian Dietz, Jan Funke,
+ * Aivar Grislis, Jonathan Hale, Grant Harris, Stefan Helfrich, Mark Hiner,
+ * Martin Horn, Steffen Jaensch, Lee Kamentsky, Larry Lindsey, Melissa Linkert,
+ * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
+ * Jean-Yves Tinevez and Michael Zinsmaier.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imglib2.util;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import net.imglib2.FinalRealInterval;
+import net.imglib2.RealInterval;
+import net.imglib2.RealPoint;
+import net.imglib2.iterator.IntervalIterator;
+import net.imglib2.iterator.LocalizingRealIntervalIterator;
+import net.imglib2.realtransform.RealTransform;
+
+/**
+ * Convenience methods for manipulating {@link RealInterval RealIntervals}.
+ *
+ * @author John Bogovic
+ *
+ */
+public class RealIntervals
+{
+	/**
+	 * Estimate the {@link RealInterval} that bounds the given RealInterval
+	 * after being transformed by a {@link RealTransform}.
+	 * <p>
+	 * This implementation estimates the bounding interval by transforming points 
+	 * on the corners of the given real interval.
+	 *
+	 * @param interval
+	 *            the real interval
+	 * @param transform
+	 *            the transformation
+	 * @return the bounding interval
+	 */
+	public static RealInterval boundingIntervalCorners( final RealInterval interval, final RealTransform transform )
+	{
+		final int nd = interval.numDimensions();
+		final double[] pt = new double[ nd ];
+
+		final double[] min = new double[ nd ];
+		final double[] max = new double[ nd ];
+		Arrays.fill( min, Double.MAX_VALUE );
+		Arrays.fill( max, Double.MIN_VALUE );
+
+		// iterate over the corners of an nd-hypercube
+		final long[] unitInterval = new long[ nd ];
+		Arrays.fill( unitInterval, 2 );
+		IntervalIterator it = new IntervalIterator( unitInterval );
+		while ( it.hasNext() )
+		{
+			it.fwd();
+			for ( int d = 0; d < nd; d++ )
+			{
+				if ( it.getLongPosition( d ) == 0 )
+					pt[ d ] = interval.realMin( d );
+				else
+					pt[ d ] = interval.realMax( d );
+			}
+
+			transform.apply( pt, pt );
+			for ( int d = 0; d < nd; d++ )
+			{
+				if ( pt[ d ] < min[ d ] )
+					min[ d ] = pt[ d ];
+
+				if ( pt[ d ] > max[ d ] )
+					max[ d ] = pt[ d ];
+			}
+		}
+
+		return new FinalRealInterval( min, max );
+	}
+
+
+	/**
+	 * Estimate the {@link RealInterval} that bounds the given RealInterval
+	 * after being transformed by a {@link RealTransform}.
+	 * <p>
+	 * This implementation estimates the bounding interval by transforming points 
+	 * on the faces of the given real interval.
+	 *
+	 * @param interval
+	 *            the real interval
+	 * @param transform
+	 *            the transformation
+	 * @param steps
+	 *            step between samples
+	 * @return the bounding interval
+	 */
+	public static RealInterval boundingIntervalFaces( final RealInterval interval, final RealTransform xfm, double... steps )
+	{
+		final int nd = interval.numDimensions();
+		final double[] min = new double[ nd ];
+		final double[] max = new double[ nd ];
+		Arrays.fill( min, Double.MAX_VALUE );
+		Arrays.fill( max, Double.MIN_VALUE );
+
+		final double[] itvlMin = new double[ nd ];
+		final double[] itvlMax = new double[ nd ];
+		for( int i = 0; i < nd; i++ )
+		{
+			interval.realMin( itvlMin );
+			interval.realMax( itvlMax );
+			itvlMin[ i ]  = interval.realMin( i );
+			itvlMax[ i ]  = interval.realMin( i );
+			transformedCoordinateBounds( xfm, new LocalizingRealIntervalIterator( itvlMin, itvlMax, steps ), min, max );
+
+			itvlMin[ i ]  = interval.realMax( i );
+			itvlMax[ i ]  = interval.realMax( i );
+			transformedCoordinateBounds( xfm, new LocalizingRealIntervalIterator( itvlMin, itvlMax, steps ), min, max );
+		}
+
+		return new FinalRealInterval( min, max );
+	}
+
+	/**
+	 * Estimate the {@link RealInterval} that bounds the given RealInterval
+	 * after being transformed by a {@link RealTransform}.
+	 * <p>
+	 * This implementation estimates the bounding interval by transforming
+	 * points in the volume of the given real interval.
+	 *
+	 * @param interval
+	 *            the real interval
+	 * @param transform
+	 *            the transformation
+	 * @param steps
+	 *            step between samples
+	 * @return the bounding interval
+	 */
+	public static RealInterval boundingIntervalVolume(
+			final RealInterval interval, final RealTransform transform, double... steps )
+	{
+		int nd = interval.numDimensions();
+		double[] min = new double[ nd ];
+		double[] max = new double[ nd ];
+		Arrays.fill( min, Long.MAX_VALUE );
+		Arrays.fill( max, Long.MIN_VALUE );
+		final LocalizingRealIntervalIterator it = new LocalizingRealIntervalIterator( interval, steps );
+		transformedCoordinateBounds( transform, it, min, max );
+		return new FinalRealInterval( min, max );
+	}
+
+	/**
+	 * Transforms all points produced by the {@link LocalizingRealIntervalIterator}
+	 * with the provided {@link RealTransform} and stores the min and max coordinates
+	 * for each dimension in the provided arrays.
+	 * <p>
+	 * Min and max arrays are not reset before iteration, meaning this method
+	 * may be called repeatedly with different iterators to find bounds of
+	 * the union of iterated points.
+	 * 
+	 * @param transform a transformation
+	 * @param it the real interval iterator
+	 * @param min the min coordinate array to modify
+	 * @param max the max coordinate array to modify
+	 */
+	protected static void transformedCoordinateBounds( 
+			final RealTransform transform, final LocalizingRealIntervalIterator it,
+			final double[] min, final double[] max )
+	{
+		final int nd = it.numDimensions();
+		final RealPoint ptxfm = new RealPoint( nd );
+		while( it.hasNext() )
+		{
+			it.fwd();
+			transform.apply( it, ptxfm );
+			for( int d = 0; d < nd; d++ )
+			{
+				final double p = ptxfm.getDoublePosition(d);
+				if( p < min[ d ])
+					min[ d ] = p;
+
+				if( p > max[ d ])
+					max[ d ] = p;
+			}
+		}
+	}
+
+}
---
+++ src/test/java/net/imglib2/util/RealIntervalsTests.java
@@ -0,0 +1,174 @@
+/*-
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2024 Tobias Pietzsch, Stephan Preibisch, Stephan Saalfeld,
+ * John Bogovic, Albert Cardona, Barry DeZonia, Christian Dietz, Jan Funke,
+ * Aivar Grislis, Jonathan Hale, Grant Harris, Stefan Helfrich, Mark Hiner,
+ * Martin Horn, Steffen Jaensch, Lee Kamentsky, Larry Lindsey, Melissa Linkert,
+ * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
+ * Jean-Yves Tinevez and Michael Zinsmaier.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imglib2.util;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import net.imglib2.FinalRealInterval;
+import net.imglib2.RealInterval;
+import net.imglib2.RealRandomAccessible;
+import net.imglib2.position.FunctionRealRandomAccessible;
+import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.realtransform.PositionFieldTransform;
+import net.imglib2.type.numeric.real.DoubleType;
+
+public class RealIntervalsTests {
+
+	static final double EPS = 1e-9;
+	static final FinalRealInterval itvl = new FinalRealInterval( 
+			new double[] { 0, 0, 0 }, new double[] { 40, 30, 20 });
+
+	@Test
+	public void testBboxCornersAffine()
+	{
+		final AffineTransform3D xfm = new AffineTransform3D();
+		xfm.scale(2, 3, 4);
+
+		final RealInterval bbox = RealIntervals.boundingIntervalCorners( itvl, xfm );
+		assertEquals( "max x ", itvl.realMax(0) * 2, bbox.realMax(0), EPS );
+		assertEquals( "max y ", itvl.realMax(1) * 3, bbox.realMax(1), EPS );
+		assertEquals( "max z ", itvl.realMax(2) * 4, bbox.realMax(2), EPS );
+	}
+
+	@Test
+	public void testBboxFacesAffine()
+	{
+		final AffineTransform3D xfm = new AffineTransform3D();
+		xfm.scale(2, 3, 4);
+
+		final RealInterval bbox = RealIntervals.boundingIntervalFaces( itvl, xfm, 10, 10, 10 );
+		assertEquals( "max x ", itvl.realMax(0) * 2, bbox.realMax(0), EPS );
+		assertEquals( "max y ", itvl.realMax(1) * 3, bbox.realMax(1), EPS );
+		assertEquals( "max z ", itvl.realMax(2) * 4, bbox.realMax(2), EPS );
+	}
+
+	@Test
+	public void testBboxVolumeAffine()
+	{
+		final AffineTransform3D xfm = new AffineTransform3D();
+		xfm.scale(2, 3, 4);
+
+		final RealInterval bbox = RealIntervals.boundingIntervalVolume( itvl, xfm, 10, 10, 10 );
+		assertEquals( "max x ", itvl.realMax(0) * 2, bbox.realMax(0), EPS );
+		assertEquals( "max y ", itvl.realMax(1) * 3, bbox.realMax(1), EPS );
+		assertEquals( "max z ", itvl.realMax(2) * 4, bbox.realMax(2), EPS );
+	}
+
+	@Test
+	public void testBboxCornersPfield()
+	{
+		final PositionFieldTransform<DoubleType> xfm = pfield();
+
+		// estimating with the corners does NOT correctly estimate the bounding box 
+		// for this transformation.  Make sure that it behaves as expected - 
+		// returning the original interval.
+		final RealInterval bbox = RealIntervals.boundingIntervalCorners( itvl, xfm );
+		assertEquals( "min x ", itvl.realMin(0), bbox.realMin(0), EPS );
+		assertEquals( "max x ", itvl.realMax(0), bbox.realMax(0), EPS );
+
+		assertEquals( "min y ", itvl.realMin(1), bbox.realMin(1), EPS );
+		assertEquals( "max y ", itvl.realMax(1), bbox.realMax(1), EPS );
+
+		assertEquals( "min z ", itvl.realMin(2), bbox.realMin(2), EPS );
+		assertEquals( "max z ", itvl.realMax(2), bbox.realMax(2), EPS );
+	}
+
+	@Test
+	public void testBboxFacesPfield()
+	{
+		final PositionFieldTransform<DoubleType> xfm = pfield();
+		final RealInterval bbox = RealIntervals.boundingIntervalFaces( itvl, xfm, 5, 5, 5 );
+		assertEquals( "min x ", itvl.realMin(0), bbox.realMin(0), EPS );
+		assertEquals( "max x ", itvl.realMax(0) + 5, bbox.realMax(0), EPS );
+
+		assertEquals( "min y ", itvl.realMin(1) - 5, bbox.realMin(1), EPS );
+		assertEquals( "max y ", itvl.realMax(1), bbox.realMax(1), EPS );
+
+		assertEquals( "min z ", itvl.realMin(2), bbox.realMin(2), EPS );
+		assertEquals( "max z ", itvl.realMax(2), bbox.realMax(2), EPS );
+	}
+
+	@Test
+	public void testBboxVolumePfield()
+	{
+		final PositionFieldTransform<DoubleType> xfm = pfield();
+		final RealInterval bbox = RealIntervals.boundingIntervalVolume( itvl, xfm, 5, 5, 5 );
+		assertEquals( "min x ", itvl.realMin(0), bbox.realMin(0), EPS );
+		assertEquals( "max x ", itvl.realMax(0) + 5, bbox.realMax(0), EPS );
+
+		assertEquals( "min y ", itvl.realMin(1) - 5, bbox.realMin(1), EPS );
+		assertEquals( "max y ", itvl.realMax(1), bbox.realMax(1), EPS );
+
+		assertEquals( "min z ", itvl.realMin(2), bbox.realMin(2), EPS );
+		assertEquals( "max z ", itvl.realMax(2), bbox.realMax(2), EPS );
+	}
+
+	private static PositionFieldTransform< DoubleType > pfield()
+	{
+		return new PositionFieldTransform<>(
+				pfieldComponent( 1, 15, 3, 0, 5),
+				pfieldComponent( 0, 20, 3, 1, -5 ),
+				pfieldIdentity( 2 ));
+	}
+
+	private static RealRandomAccessible<DoubleType> pfieldComponent( final int dim, final double pos, final double width, 
+			final int outdim, final double amount )
+	{
+		return new FunctionRealRandomAccessible<>( 3,
+				(p,v) -> {
+					final double pd = p.getDoublePosition( dim );
+					final double outp = p.getDoublePosition( outdim );
+					if( pd >= pos - width && pd < pos + width )
+						v.set( outp + amount );
+					else 
+						v.set( outp );
+				},
+				DoubleType::new );
+	}
+
+	/**
+	 * The identity position field for a specified dimension
+	 * 
+	 * @param dim the dimension
+	 * @return the field
+	 */
+	private static RealRandomAccessible<DoubleType> pfieldIdentity( final int dim )
+	{
+		return new FunctionRealRandomAccessible<>( 3,
+				(p,v) -> { v.set( p.getDoublePosition( dim )); },
+				DoubleType::new );
+	}
+
+}
