97bc70a91bd1f082fa31b910d5c5a1d0bde01a4a
John Bogovic <bogovicj@janelia.hhmi.org>
2025-06-18 14:48:15 -0400

    test: better test parameters for SphericalCurvatureZDistortion

--- src/test/java/net/imglib2/realtransform/SphericalCurvatureZDistortionTest.java
+++ src/test/java/net/imglib2/realtransform/SphericalCurvatureZDistortionTest.java
@@ -14,8 +14,8 @@ public class SphericalCurvatureZDistortionTest
 	@Test
 	public void inverseTest() throws Exception
 	{
-		final SphericalCurvatureZDistortion tform = new SphericalCurvatureZDistortion( 3, 2, 100.0 );
-		final FinalRealInterval interval = Intervals.createMinMaxReal( -1.0, -1.0, 98.0, 2.01, 2.01, 99.01 );
+		final SphericalCurvatureZDistortion tform = new SphericalCurvatureZDistortion( 3, 2, 10000.0 );
+		final FinalRealInterval interval = Intervals.createMinMaxReal( -1.0, -1.0, -1.0, 2.01, 2.01, 2.01 );
 		final double[] step = new double[] { 0.1, 0.1, 0.1 };
 
 		IterableInverseTests.inverseTransformTestHelper( tform, interval, step, EPS );
