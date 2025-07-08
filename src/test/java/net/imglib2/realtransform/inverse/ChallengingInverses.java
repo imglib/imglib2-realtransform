dbb00fee22221b25b5c40d12d4f5e3d052c034a1
John Bogovic <bogovicj@janelia.hhmi.org>
2025-07-08 18:14:45 -0400

    test: update ChallengingInverseTest

--- src/test/java/net/imglib2/realtransform/inverse/ChallengingInverses.java
+++ src/test/java/net/imglib2/realtransform/inverse/ChallengingInverses.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import net.imglib2.realtransform.InvertibleRealTransform;
 import net.imglib2.realtransform.ThinplateSplineTransform;
 
 public class ChallengingInverses
@@ -35,43 +36,56 @@ public class ChallengingInverses
 	@Test
 	public void testInv() {
 
-		final ThinplateSplineTransform tps = new ThinplateSplineTransform( srcPts, tgtPts );
+		final ThinplateSplineTransform tps = new ThinplateSplineTransform( tgtPts, srcPts );
 		final WrappedIterativeInvertibleRealTransform tf = new WrappedIterativeInvertibleRealTransform( tps );
-
-		// Configure optimizer for this challenging case
-		InverseRealTransformGradientDescent optimizer = tf.getOptimzer();
-		optimizer.setTolerance( 0.001 );
-		optimizer.setMaxIters( 2000 );
-		optimizer.setBeta( 0.5 );
+		InverseRealTransformGradientDescent opt = tf.getOptimzer();
+		opt.setBeta(0.5);
+		opt.setMaxIters(2000);
+		opt.setTolerance(0.0001);
 
 		int idx = 0;
 		final double[] p0 = new double[] { srcPts[0][idx], srcPts[1][idx] };
 		final double[] q0 = new double[] { tgtPts[0][idx], tgtPts[1][idx] };
 		final double[] res = new double[nd];
+		final double[] resXfm = new double[nd];
 
+		testInverseAndRoundTrip( tf, p0, q0, res, resXfm );
+
+		q0[0] = 0;
+		q0[1] = 0;
+		testInverseAndRoundTrip( tf, p0, q0, res, resXfm );
+
+		q0[0] = 50;
+		q0[1] = 120;
+		testInverseAndRoundTrip( tf, p0, q0, res, resXfm );
+	}
+
+	private static void testInverseAndRoundTrip( InvertibleRealTransform tf, double[] p0, double[] q0, double[] res, double[] resXfm ) {
 		tf.applyInverse( res, q0 );
-		assertArrayEquals(p0, res, 0.1);
+		tf.apply( res, resXfm );
+		assertArrayEquals(p0, res, 1.0);
+		assertArrayEquals(q0, resXfm, 1.0);
 	}
 
 	private static double[][] transpose(double[][] matrix) {
-	    if (matrix == null || matrix.length == 0) {
-	        return new double[0][0];
-	    }
-	    
-	    int rows = matrix.length;
-	    int cols = matrix[0].length;
-	    
-	    // Create transposed matrix with swapped dimensions
-	    double[][] transposed = new double[cols][rows];
-	    
-	    // Fill the transposed matrix
-	    for (int i = 0; i < rows; i++) {
-	        for (int j = 0; j < cols; j++) {
-	            transposed[j][i] = matrix[i][j];
-	        }
-	    }
-	    
-	    return transposed;
+
+		if (matrix == null || matrix.length == 0) {
+			return new double[0][0];
+		}
+
+		// Create transposed matrix with swapped dimensions
+		int rows = matrix.length;
+		int cols = matrix[0].length;
+		final double[][] transposed = new double[cols][rows];
+
+		// Fill the transposed matrix
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				transposed[j][i] = matrix[i][j];
+			}
+		}
+
+		return transposed;
 	}
 	
 }
