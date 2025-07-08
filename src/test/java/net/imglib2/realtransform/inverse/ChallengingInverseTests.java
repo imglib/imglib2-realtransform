6e37ce902faa8ccb5599208bbc7026a17dcffe96
John Bogovic <bogovicj@janelia.hhmi.org>
2025-07-08 18:40:58 -0400

    test: update ChallengingInverseTests

--- src/test/java/net/imglib2/realtransform/inverse/ChallengingInverseTests.java
+++ src/test/java/net/imglib2/realtransform/inverse/ChallengingInverseTests.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import net.imglib2.realtransform.InvertibleRealTransform;
 import net.imglib2.realtransform.ThinplateSplineTransform;
 
-public class ChallengingInverses
+public class ChallengingInverseTests
 {
 	final int nd = 2;
 
@@ -40,8 +40,8 @@ public class ChallengingInverses
 		final WrappedIterativeInvertibleRealTransform tf = new WrappedIterativeInvertibleRealTransform( tps );
 		InverseRealTransformGradientDescent opt = tf.getOptimzer();
 		opt.setBeta(0.5);
-		opt.setMaxIters(2000);
-		opt.setTolerance(0.0001);
+		opt.setMaxIters(200000);
+		opt.setTolerance(0.001);
 
 		int idx = 0;
 		final double[] p0 = new double[] { srcPts[0][idx], srcPts[1][idx] };
@@ -53,18 +53,18 @@ public class ChallengingInverses
 
 		q0[0] = 0;
 		q0[1] = 0;
-		testInverseAndRoundTrip( tf, p0, q0, res, resXfm );
+		testInverseAndRoundTrip( tf, null, q0, res, resXfm );
 
 		q0[0] = 50;
 		q0[1] = 120;
-		testInverseAndRoundTrip( tf, p0, q0, res, resXfm );
+		testInverseAndRoundTrip( tf, null, q0, res, resXfm );
 	}
 
 	private static void testInverseAndRoundTrip( InvertibleRealTransform tf, double[] p0, double[] q0, double[] res, double[] resXfm ) {
+
 		tf.applyInverse( res, q0 );
 		tf.apply( res, resXfm );
-		assertArrayEquals(p0, res, 1.0);
-		assertArrayEquals(q0, resXfm, 1.0);
+		assertArrayEquals( q0, resXfm, 0.5 );
 	}
 
 	private static double[][] transpose(double[][] matrix) {
